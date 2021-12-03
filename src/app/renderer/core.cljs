a15488e4e23fe6a9dc58ccd55c4a2c8c8a1c7202
Henrik Stromblad <henrik.stromblad@se.abb.com>
2021-12-03 12:56:06 +0100

    Fixed viewbox issue

--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -26,14 +26,14 @@
   [:div.text-gray-800
    (condp = icon
      :library
-     [:svg {:class "w-6 h-6", :fill "none", :stroke "currentColor", :viewbox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg"}
+     [:svg {:class "w-6 h-6", :fill "none", :stroke "currentColor", :viewBox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg"}
       [:path {:stroke-linecap "round", :stroke-linejoin "round", :stroke-width "2", :d "M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"}]]
 
      :home
-     [:svg {:class "w-6 h-6", :fill "none", :stroke "currentColor", :viewbox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg"}
+     [:svg {:class "w-6 h-6", :fill "none", :stroke "currentColor", :viewBox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg"}
       [:path {:stroke-linecap "round", :stroke-linejoin "round", :stroke-width "2", :d "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"}]]
 
-     [:svg {:class "w-6 h-6", :fill "none", :stroke "currentColor", :viewbox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg"}
+     [:svg {:class "w-6 h-6", :fill "none", :stroke "currentColor", :viewBox "0 0 24 24", :xmlns "http://www.w3.org/2000/svg"}
       [:path {:stroke-linecap "round", :stroke-linejoin "round", :stroke-width "2", :d "M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"}]])]
   )
 
